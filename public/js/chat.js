a194f3d3d0d63f4c0087d7c84a61c77e090f8278
mostafasobh <mostafasobh33@gmail.com>
2019-12-20 00:25:35 -0800

    Add join page that submits chat.html

--- public/js/chat.js
+++ public/js/chat.js
@@ -7,7 +7,7 @@ var socket = io();
         //Heights
         var clientHeight=messages.prop('clientHeight');
         var scrollTop=messages.prop('scrollTop');
-        var scrollHeight=messages.prop('scrollHeight');
+        var scrollHeight=messages.prop('scrollHe ight');
         var newMessageHeight=newMessage.innerHeight()
         var lastMessageHeight=newMessage.prev().innerHeight()
         if(clientHeight+ newMessageHeight + lastMessageHeight + scrollTop >=scrollHeight){
